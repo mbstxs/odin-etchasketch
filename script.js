21697543c0a75d58a4de8063fdec8d2aad8a9f57
Marcelo Atkins <xxatkinsxx@gmail.com>
2025-08-15 00:40:42 +0100

    feat (grid): random rgb on hover

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ for (let i = 0; i <= 255; i++) {
     div.style.width = "80px";
     div.style.height = "48px";
     div.style.border = "1px solid #000";
-    div.addEventListener("mouseenter", () => { div.style.backgroundColor = "#000" })
+    let randomR = Math.floor(Math.random() * 255);
+    let randomG = Math.floor(Math.random() * 255);
+    let randomB = Math.floor(Math.random() * 255);
+    div.addEventListener("mouseenter", () => { div.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})` })
     div.addEventListener("mouseleave", () => { div.style.backgroundColor = "#fff" })
     cont.appendChild(div);
 }
